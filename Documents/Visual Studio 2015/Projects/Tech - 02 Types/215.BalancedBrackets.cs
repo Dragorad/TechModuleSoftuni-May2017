661cb1641704921641c862297afe4498c382d56b
Dragorad <mi.e.drago@gmail.com>
2017-06-05 22:52:53 +0300

    Add files via upload

---
+++ Documents/Visual Studio 2015/Projects/Tech - 02 Types/215.BalancedBrackets.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _215.BalancedBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string output = "good";
+            string toRemove = "()";
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                if (input.Equals("(") || input.Equals(")"))
+                {
+                    output = output + input;
+
+
+                }
+
+            }
+            output = output.Replace(toRemove, "");
+            string answer = "";
+            if (output.Equals("good"))
+            {
+                answer = "BALANCED";
+
+            }
+            else
+            {
+                answer = "UNBALANCED";
+            }
+
+            Console.WriteLine(answer);
+        }
+    }
+}
